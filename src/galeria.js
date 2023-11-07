739adabef460b2032e8e7a70375568bcf5db0393
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-07 18:42:17 +0100

    React 7.11 proba chmurowa

---
+++ src/galeria.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import './galeriacss.css';
+import './chmurkascss.css'
+
+const Galeria = () => {
+    const obrazy = [
+        { src: '/zdjecia/zdjecie1.jpg', title: 'Pierwszy obrazek', description: 'Opis pierwszego obrazka' },
+        { src: '/zdjecia/zdjecie2.jpg', title: 'Drugi obrazek', description: 'Opis drugiego obrazka' },
+        { src: '/zdjecia/zdjecie3.jpg', title: 'Trzeci obrazek', description: 'Opis trzeciego obrazka' },
+        { src: '/zdjecia/zdjecie4.jpg', title: 'Czwarty obrazek', description: 'Opis czwartego obrazka' },
+        { src: '/zdjecia/zdjecie5.jpg', title: 'Piąty obrazek', description: 'Opis piątego obrazka' },
+        { src: '/zdjecia/zdjecie6.jpg', title: 'Szósty obrazek', description: 'Opis szóstego obrazka' },
+        { src: '/zdjecia/zdjecie7.jpg', title: 'Siódmy obrazek', description: 'Opis siódmego obrazka' },
+    ];
+
+    const [hoveredInfo, setHoveredInfo] = useState(null);
+
+    const handleImageHover = (image) => {
+        setHoveredInfo(image);
+    };
+
+    const handleImageLeave = () => {
+        setHoveredInfo(null);
+    }
+
+    return (
+        <div className="galeria">
+            {obrazy.map((image, index) => (
+                <div
+                    key={index + 1}
+                    className="image-container"
+                    onMouseEnter={() => handleImageHover(image)}
+                    onMouseLeave={handleImageLeave}
+                >
+                    <img src={image.src} alt={image.title} />
+                    {hoveredInfo === image && (
+                        <div className="image-info">
+                            <h3>{image.title}</h3>
+                            <p>{image.description}</p>
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Galeria;
