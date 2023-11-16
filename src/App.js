89b8524639725a7317b4ee0d8d9b9a561bc2b38b
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-11-16 16:42:55 +0100

    React 16.11 menu nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Galeria from "./galeria";
 function App() {
   return (
     <div className="App">
+        <div className="Menu">
+        <button>Strona Główna</button>
+        <button>Obrazy</button>
+        <button>Dodaj obraz</button>
+        <button>Kontakt</button>
+        </div>
         <h1>Witamy w Galerii</h1>
         <Galeria/>
     </div>
