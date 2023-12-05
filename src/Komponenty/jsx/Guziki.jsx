e52d207af03b3388e3b2183bccfce2c7f8ca0e2c
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-12-05 14:47:52 +0100

    React 5.12 Wystylowane nowe buttony

--- src/Komponenty/jsx/Guziki.jsx
+++ src/Komponenty/jsx/Guziki.jsx
@@ -1,19 +1,19 @@
 // MenuButtons.jsx
 
 import React from "react";
-import {Link, Route, Router, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import "../css/Guziki.css"
 
-function MenuButtons() {
+function Guziki() {
     return (
         <div className="Menu">
-            <Link to="/" className="Menu button ">Strona Główna</Link>
-            <Link to="/obrazy" className="Menu button ">Obrazy</Link>
-            <Link to="/dodaj-obraz" className="Menu button ">Dodaj obraz</Link>
-            <Link to="/Kontakt" className="Menu button ">Kontakt</Link>
+            <Link to="/" className="button ">Strona Główna</Link>
+            <Link to="/obrazy" className="button ">Obrazy</Link>
+            <Link to="/dodaj-obraz" className="button ">Dodaj obraz</Link>
+            <Link to="/Kontakt" className="button ">Kontakt</Link>
         </div>
     );
 }
 
 
-export default MenuButtons;
+export default Guziki;
