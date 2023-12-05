ffac5a5a0e8ac623e342c1b3c87a2e1557ed7c7b
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-12-05 23:33:17 +0100

    React 5.12 praca nad Strona Kontaktu, dodane pole txt

--- src/Komponenty/jsx/Kontakt.jsx
+++ src/Komponenty/jsx/Kontakt.jsx
@@ -1,25 +1,28 @@
-import "../css/Kontakt.css"
+// Kontakt.jsx
 
-const Kontakt = ()=>{
-    return(
+import React from "react";
+import "../css/Kontakt.css"; // Importuje plik CSS dla Komponentu Kontakt
+
+const Kontakt = () => {
+    return (
         <div className="form">
             <h1>Kontakt</h1>
             <div className="ButtonSend">
                 <div className="Wiadomosc">
-
-            </div>
-
+                    <label htmlFor="message">Wiadomość:</label>
+                    <textarea id="message" name="message" rows="4" cols="50"></textarea>
+                </div>
                 <h3 className="SendButton">Wyślij</h3>
-
             </div>
             <div>
                 <h1>Faq</h1>
-                    <ul className="FAQlist">
+                <ul className="FAQlist">
                     <li className="FAQ"> asdsdghsdfgh</li>
                     <li className="FAQ">dgjfgjkghjk</li>
                 </ul>
             </div>
         </div>
-    )
-}
-export default Kontakt
+    );
+};
+
+export default Kontakt;
