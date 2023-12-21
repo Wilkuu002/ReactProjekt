a447c4a1af5d47dc42002bca0a4f42158698f694
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2023-12-21 16:39:30 +0100

    React 21.12 Komunikat wyslij waidomośc, czyszczenie textu

--- src/Komponenty/jsx/Kontakt.jsx
+++ src/Komponenty/jsx/Kontakt.jsx
@@ -1,18 +1,31 @@
 
-import React from "react";
+import React, {useState} from "react";
 import "../css/Kontakt.css";
 
 const Kontakt = () => {
+    const [wiadomoscWyslana, setWiadomoscWyslana] = useState(false);
+    const [wiadomosc, setWiadomosc] = useState("");
+
+    const handleWyślij = () => {
+        setWiadomoscWyslana(true);
+        setWiadomosc("");
+    };
+
     return (
         <div className="form">
             <h1 className="Naglowki">Kontakt</h1>
             <div className="ButtonSend">
                 <div className="Wiadomosc">
+                    {wiadomoscWyslana && <p>Wiadomość została wysłana!</p>}
                     <div>Wiadomość</div>
                     <label htmlFor="message"></label>
-                    <textarea id="message" name="message" rows="4" cols="50"></textarea>
+                    <textarea id="message" name="message" rows="4" cols="50"
+                     value={wiadomosc}
+                    onChange={(e)=>setWiadomosc(e.target.value)}></textarea>
                 </div>
-                <h3 className="SendButton">Wyślij</h3>
+                <button className="SendButton" onClick={handleWyślij}>
+                    Wyślij
+                </button>
             </div>
             <div>
                 <h1 className="Naglowki">Faq</h1>
